cb6a0805c9c3c45223fa6cea39983d98952c121a
suzuya0 <sakura240613@gmail.com>
2021-10-25 13:04:16 +0000

    [Fix]applicaton.js修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,6 @@
 class HomesController < ApplicationController
   def top
-    # @bartered_items = BarteredItem.where(is_deleted: false).last(4).reverse
-    @bartered_items = BarteredItem.all
+    @bartered_items = BarteredItem.where(is_deleted: false).last(4).reverse
   end
 
   def about
