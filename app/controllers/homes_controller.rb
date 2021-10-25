7a4f91942b0ea1ffe0dceb74c7f015cbfcdf153d
suzuya0 <sakura240613@gmail.com>
2021-10-25 12:08:29 +0000

    [Fix]homesコントローラー]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,7 @@
 class HomesController < ApplicationController
   def top
-    @bartered_items = BarteredItem.where(is_deleted: false).last(4).reverse
+    # @bartered_items = BarteredItem.where(is_deleted: false).last(4).reverse
+    @bartered_items = BarteredItem.all
   end
 
   def about
